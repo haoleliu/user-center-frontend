28f6f6d7d50a1fdd29ac59f9a0c23be1d567a525
liu haole <2874511255@qq.com>
2024-07-30 16:17:22 +0800

    second commit

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -15,9 +15,9 @@ const Settings: ProLayoutProps & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: '乐泡泡用户中心',
   pwa: true,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  logo: 'https://typora-vastjoy.oss-cn-shanghai.aliyuncs.com/logo.png',
   iconfontUrl: '',
   token: {
     // 参见ts声明，demo 见文档，通过token 修改样式
